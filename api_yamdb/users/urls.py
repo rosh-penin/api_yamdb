f9956f3c736b8602bd4253510fdda3521e629b4a
Илья Малашенко <melax08@yandex.ru>
2022-11-14 23:50:37 +0400

    Small fix

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet, SignUpViewSet, CustomTokenObtainPairView
+from .views import UserViewSet, SignUpViewSet, custom_token_obtain
 
 app_name = 'users'
 
@@ -11,6 +11,6 @@ router_v1.register(r'auth/signup', SignUpViewSet)
 router_v1.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('v1/auth/token/', CustomTokenObtainPairView.as_view()),
+    path('v1/auth/token/', custom_token_obtain, name='token_obtain'),
     path('v1/', include(router_v1.urls))
 ]
