73dc7468c3b6a8e8837235c8627d5167af11d3b1
Покровский Сергей <sergee13@yandex.ru>
2022-11-15 23:16:05 +0300

    comments to main urls

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -6,10 +6,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls', namespace='users')),
+    # Conflict here. Move your urls in api app.
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    # path('', include('api.urls'), name='api')
+    # path('/api', include('api.urls'), name='api')
+    # And uncomment line above.
 ]
