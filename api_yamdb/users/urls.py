83bb04a9122e93648914e49ab74377b265aafd19
Илья Малашенко <melax08@yandex.ru>
2022-11-16 12:22:20 +0400

    Optimize APIViews if statements

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet, UsersSignUp, UsersToken
+from .views import UserViewSet, UsersSignUp, UsersTokenObtain
 
 app_name = 'users'
 
@@ -10,7 +10,7 @@ router_v1 = routers.DefaultRouter()
 router_v1.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('v1/auth/token/', UsersToken.as_view(), name='token_obtain'),
+    path('v1/auth/token/', UsersTokenObtain.as_view(), name='token_obtain'),
     path('v1/auth/signup/', UsersSignUp.as_view(), name='signup'),
     path('v1/', include(router_v1.urls))
 ]
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import get_object_or_404
-from django.utils.crypto import get_random_string
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.utils.crypto import get_random_string
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import (UserSerializer,
                           SignUpSerializer,
                           CustomTokenObtainSerializer
@@ -36,36 +37,34 @@ class UsersSignUp(APIView):
 
     def post(self, request):
         serializer = SignUpSerializer(data=request.data)
-        if serializer.is_valid():
-            username = serializer.data.get('username')
-            email = serializer.data.get('email')
-            confirmation_code = get_random_string(length=50)
-            user, created = User.objects.get_or_create(username=username,
-                                                       email=email)
-            send_confirmation_code([email], confirmation_code)
-            user.confirmation_code = confirmation_code
-            user.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get('username')
+        email = serializer.data.get('email')
+        confirmation_code = get_random_string(length=50)
+        user, created = User.objects.get_or_create(username=username,
+                                                   email=email)
+        send_confirmation_code([email], confirmation_code)
+        user.confirmation_code = confirmation_code
+        user.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class UsersToken(APIView):
+class UsersTokenObtain(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
         serializer = CustomTokenObtainSerializer(data=request.data)
-        if serializer.is_valid():
-            username = serializer.data.get('username')
-            confirmation_code = serializer.data.get('confirmation_code')
-            user = get_object_or_404(User, username=username)
-            if user.confirmation_code == confirmation_code:
-                user.confirmation_code = None
-                user.save()
-                return Response(get_access_token(user),
-                                status=status.HTTP_200_OK)
-            return Response({'confirmation_code': 'is invalid'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get('username')
+        confirmation_code = serializer.data.get('confirmation_code')
+        user = get_object_or_404(User, username=username)
+        if user.confirmation_code == confirmation_code:
+            user.confirmation_code = None
+            user.save()
+            return Response(get_access_token(user),
+                            status=status.HTTP_200_OK)
+        return Response({'confirmation_code': 'is invalid'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(viewsets.ModelViewSet):
