cb2afc957dfc32ed31aab2d7a35e006307ed8e99
Илья Малашенко <melax08@yandex.ru>
2022-11-17 11:54:52 +0400

    Fix some permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return (request.user.role == 'admin' if request.user.is_authenticated
-                else False or request.user.is_superuser)
+        return (request.user.role == 'admin' or request.user.is_superuser
+                if request.user.is_authenticated else False)
 
 
 class IsAdminOrReadOnly(BasePermission):
@@ -19,7 +19,8 @@ class IsAdminOrModerOrAuthorOrReadOnly(BasePermission):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS or
-                (request.user.role == 'moderator'
-                 or request.user == obj.author or request.user.is_superuser)
-                if request.user.is_authenticated else False)
+        return (request.method in SAFE_METHODS
+                or request.user.role == 'moderator'
+                or request.user.role == 'admin'
+                or request.user == obj.author or request.user.is_superuser)
+
