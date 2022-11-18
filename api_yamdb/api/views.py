aa5d503377b1fc2b4e2fdb2f001162f183c2ced3
Artyom Buksha <artium2207@gmail.com>
2022-11-18 12:01:47 +0300

    Validation for reviews & get_object refactoring

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
                                    ListModelMixin)
@@ -13,7 +14,6 @@ from .serializers import (CategorySerializer, GenreSerializer,
 
 
 def get_object(self, keyword, model):
-
     return get_object_or_404(model, pk=self.kwargs.get(keyword))
 
 
@@ -49,19 +49,16 @@ class ReviewViewSet(ModelViewSet):
     permission_classes = (IsAdminOrModerOrAuthorOrReadOnly, )
 
     def get_queryset(self):
-        # title_id = self.kwargs.get('title_id')
-        # title = get_object_or_404(Title, pk=title_id)
         title = get_object(self, 'title_id', Title)
-
         return title.reviews.all()
 
     def perform_create(self, serializer):
-        # title_id = self.kwargs.get('title_id')
-        # title = get_object_or_404(Title, pk=title_id)
         title = get_object(self, 'title_id', Title)
         serializer.save(
             author=self.request.user,
             title=title)
+        if self.request.user.reviews.filter(title=title):
+            raise ValidationError('You already posted review to this title')
 
 
 class CommentViewSet(ModelViewSet):
@@ -69,15 +66,11 @@ class CommentViewSet(ModelViewSet):
     permission_classes = (IsAdminOrModerOrAuthorOrReadOnly, )
 
     def get_queryset(self):
-        # review_id = self.kwargs.get('review_id')
-        # review = get_object_or_404(Review, pk=review_id)
         review = get_object(self, 'review_id', Review)
 
         return review.comments.all()
 
     def perform_create(self, serializer):
-        # review_id = self.kwargs.get('review_id')
-        # review = get_object_or_404(Review, pk=review_id)
         review = get_object(self, 'review_id', Review)
         serializer.save(
             author=self.request.user,
